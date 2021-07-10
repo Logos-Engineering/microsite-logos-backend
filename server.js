60d8fc3b8a3f4c6dc3480505260c4bc0d569b4a3
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2021-07-10 23:15:44 +0700

    inisialisasi project

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -8,14 +8,12 @@ const indexRouter = require('./routes/index');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// view engine setup
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/', indexRouter);
+// Routes
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -24,15 +22,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  
+  res.status(err.status || 500).json({
+    message: 'Not found'
+  });
 });
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
