67ddaedd07757ea98d0c648c3b50f33c650172e5
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2021-08-10 19:16:48 +0700

    setup data validator

---
+++ src/middlewares/validator/index.js
@@ -0,0 +1,22 @@
+const { validationResult } = require('express-validator');
+
+const { ClientErrors } = require('../../middlewares/error');
+
+function validator(req, res, next) {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const arr = errors.array().map((obj) => obj.msg);
+      const error = new ClientErrors(arr);
+      throw error;
+    }
+
+    next();
+    
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = { validator }
