657eb497c193f77cf0fdcd022c6f4b0d646583a4
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2021-08-15 21:18:20 +0700

    menambahkan validator panjang string pada skema validasi

--- src/middlewares/validator/schema.js
+++ src/middlewares/validator/schema.js
@@ -9,18 +9,31 @@ const optUrl = {
 
 // skema untuk data link
 const linkSchema = [
-  check('name').exists({ checkFalsy: true, checkNull: true }).isString().withMessage('The link name is required and it must be string type'),
-  check('link').exists({ checkFalsy: true, checkNull: true }).isURL(optUrl).withMessage('The link is required and it must be URL type'),
-  check('publish').exists({ checkFalsy: true, checkNull: true }).isBoolean().toBoolean()
+  check('name').exists({ checkFalsy: true, checkNull: true }).isString()
+    .isLength({ min: 2, max: 50 })
+    .withMessage('Link name is required and it must be string type which length of string at least 2 characters'),
+  check('link').exists({ checkFalsy: true, checkNull: true }).isURL(optUrl)
+    .isLength({ min: 2, max: undefined })
+    .withMessage('Link is required and it must be URL type which length of string at least 2 characters'),
+  check('publish').exists({ checkFalsy: true, checkNull: true }).isBoolean()
+    .toBoolean()
     .withMessage('Publish field is required and it must be boolean type'),
-  check('category').exists({ checkFalsy: true, checkNull: true }).isString().withMessage('The link category is required and it must be string type'),
-  check('titleWebinar').optional({ checkFalsy: true }).isString().withMessage('The webinar title is required and it must be string type'),
-  check('summaryWebinar').optional({ checkFalsy: true }).isString().withMessage('The webinar summary is required and it must be string type'),
+  check('category').exists({ checkFalsy: true, checkNull: true }).isString()
+    .isLength({ min: 2, max: 100 })
+    .withMessage('Link category is required and it must be string type which length of string at least 2 characters'),
+  check('titleWebinar').optional({ checkFalsy: true }).isString()
+    .isLength({ min: 2, max: 300 })
+    .withMessage('Webinar title is required and it must be string type which length of string at least 2 characters'),
+  check('summaryWebinar').optional({ checkFalsy: true }).isString()
+    .isLength({ min: 5, max: undefined })
+    .withMessage('Webinar summary is required and it must be string type which length of string at least 5 characters'),
 ];
 
 // skema untuk data user
 const userSchema = [
-  check('username').exists({ checkFalsy: true, checkNull: true }).isString().withMessage('Username is required and it must be string type'),
+  check('username').exists({ checkFalsy: true, checkNull: true }).isString()
+    .isLength({ min: 5, max: 20 })
+    .withMessage('Username is required and it must be string type which length of string at least 5 characters'),
 ];
 
 module.exports = { linkSchema, userSchema };
