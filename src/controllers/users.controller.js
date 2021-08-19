a722d0d8ff79d0a5a54735a92747c8c11619c78d
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2021-08-19 19:24:41 +0700

    menambahkan controller untuk fitur READ data users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -29,6 +29,23 @@ async function postUserController(req, res, next) {
   }
 }
 
+async function getUsersController(req, res, next) {
+  try {
+    const users = await model.User.findAll({
+      attributes: ['id', 'username'],
+    });
+    res.status(200);
+    res.json({
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   postUserController,
+  getUsersController,
 };
