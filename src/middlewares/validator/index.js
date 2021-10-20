580d1521c75a01e1c3a4e121fb2f5be4415b9828
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2021-10-20 23:15:44 +0700

    delete: menghapus statement untuk menghapus file

--- src/middlewares/validator/index.js
+++ src/middlewares/validator/index.js
@@ -1,5 +1,4 @@
 const { validationResult } = require('express-validator');
-const fs = require('fs');
 
 const { ClientErrors } = require('../error');
 
@@ -8,16 +7,6 @@ function validator(req, res, next) {
     const errors = validationResult(req);
     // jika terdapat data yang tidak sesuai dengan skema validasi
     if (!errors.isEmpty()) {
-      // jika user mengunggah gambar
-      if (req.file) {
-        // hapus gambar tersebut
-        fs.unlink(`${process.cwd()}/${req.file.path}`, (err) => {
-          if (err) {
-            throw new Error('Internal server error');
-          }
-        });
-      }
-
       const arr = errors.array().reduce((acc, { param, msg }) => ({ ...acc, [param]: msg }), {});
       const error = new ClientErrors(arr);
       throw error;
